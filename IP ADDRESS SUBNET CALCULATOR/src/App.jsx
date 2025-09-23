af05c508911de165a39782558022facfa318e494
gokulsjk2005 <krishna05sjk@gmail.com>
2025-09-23 22:28:35 +0530

    Added some responsiveness

--- IP ADDRESS SUBNET CALCULATOR/src/App.jsx
+++ IP ADDRESS SUBNET CALCULATOR/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 import SignUpPage from './Components/SignUpPage';
 import Home from './Components/Home';
